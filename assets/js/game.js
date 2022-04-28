b8148ae3c8a3f013f8128121b017a11ee6438c5e
Rolo2121 <jakebauer21@gmail.com>
2022-04-28 14:23:28 -0500

    shop functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,6 +32,13 @@ var startGame = function() {
 
             enemyHealth = 50;
             fight(pickedEnemyName);
+            if (playerHealth > 0 && i < enemyNames.length - 1){
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next roung?");
+                if (storeConfirm) {
+
+                    shop();
+                }
+            }
         }
         else {
             window.alert(" You have lost your robot in battle! Game Over!");
@@ -58,6 +65,45 @@ var endGame = function() {
              }
      }   
 }
+
+var shop = function() {
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    switch (shopOptionPrompt) {
+        case "refill":
+        case "REFILL":
+            if (playerMoney >= 7) {
+                window.alert("Refilling player's health by 20 for 7 dollars.");
+                playerHealth = playerHealth + 20
+                playerMoney = playerMoney - 7;
+                }
+                else{
+                    window.alert("You don't have enough money!");
+                }
+                break;
+        case "upgrade":
+        case "UPGRADE":
+            if (playerMoney >= 7) {
+                window.alert("Upgrading player's attack by 6 for 7 dollars.");
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+                }
+                else{
+                    window.alert("You don't have enough money!");
+                }
+                break;
+        case "leave":
+        case "LEAVE":
+                window.alert("Leaving the shop.");
+                break;
+                default:
+                window.alert("You should pick a valid option. Try again?");
+                shop();
+                break;
+        
+    }
+};
 
                  // Fight Function
 var fight = function(enemyName) {
